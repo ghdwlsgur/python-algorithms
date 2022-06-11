6debaf08d46053f539a94136c385fecbab1862c1
HongJinHyeok <redmax45@naver.com>
2022-06-11 19:32:08 +0900

    👔 backjoon 1단계

---
+++ 1/8.py
@@ -0,0 +1,6 @@
+num = input().split()
+print(int(num[0]) + int(num[1]))
+print(int(num[0]) - int(num[1]))
+print(int(num[0]) * int(num[1]))
+print(int(num[0]) // int(num[1]))
+print(int(num[0]) % int(num[1]))
