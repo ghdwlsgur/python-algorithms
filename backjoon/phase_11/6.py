541e830e9ee1ad3ca1cf834a851e3e4de52c2806
HongJinHyeok <redmax45@naver.com>
2022-06-21 22:49:30 +0900

    👔 baekjoon 11650번

--- backjoon/phase_11/6.py
+++ backjoon/phase_11/6.py
@@ -1,4 +1,28 @@
 
+# ! version 1
+# l = []
+# for _ in range(int(input())):
+#     l.append(list(map(int, input().split())))
+# l.sort()
+# for a, b in l:
+#     print(a, b)
 
-for _ in range(int(input())):
-    a, b = map(int, input().split())
+# ! version 2
+# N = int(sys.stdin.readline())
+# M = [list(map(int, sys.stdin.readline().split())) for i in range(N)]
+# M.sort()
+# for a, b in M:
+#     print(a, b)
+
+# ! version 3
+# n = int(sys.stdin.readline())
+# l = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
+# l.sort()
+# for a, b in l:
+#     print(a, b)
+
+import sys
+l = sorted([list(map(int, sys.stdin.readline().split()))
+           for i in range(int(sys.stdin.readline()))])
+for i in l:
+    print(l[i][0], l[i][1])
