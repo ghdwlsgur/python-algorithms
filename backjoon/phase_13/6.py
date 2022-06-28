7dd1496c2c713a76a22627a5d1155769d8af2a3e
HongJinHyeok <redmax45@naver.com>
2022-06-28 13:55:36 +0900

    ♻️

--- backjoon/phase_13/6.py
+++ backjoon/phase_13/6.py
@@ -218,3 +218,17 @@ for _ in range(n):
 #     print(2)
 #   else:
 #     print(0)
+
+# n = int(sys.stdin.readline().rstrip())
+
+# for _ in range(n):
+#     x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
+#     distance = sqrt(abs(x1 - x2) ** 2 + abs(y1 - y2) ** 2)
+#     if distance == 0 and r1 == r2:
+#         print(-1)
+#     elif abs(r1 - r2) == distance or r1 + r2 == distance:
+#         print(1)
+#     elif abs(r1 - r2) < distance < (r1 + r2):
+#         print(2)
+#     else:
+#         print(0)
