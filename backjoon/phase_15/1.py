3851ae5d95f2d79c2894242879f40f75bb6b302b
HongJinHyeok <redmax45@naver.com>
2022-07-04 15:27:33 +0900

    👔 baekjoon 15649번

---
+++ backjoon/phase_15/1.py
@@ -0,0 +1,29 @@
+
+# import sys
+# from itertools import permutations
+# N, M = map(int, sys.stdin.readline().split())
+
+# cache = []
+# for i in range(1, N+1):
+#     if M == 1:
+#         print(i)
+#     else:
+#         cache.append(i)
+
+# for n in permutations(cache, M):
+#     print(*n)
+
+# ! 라이브러리 사용
+# import sys
+# from itertools import permutations
+# N, M = map(int, sys.stdin.readline().split())
+
+# cache = []
+# for i in range(1, N + 1):
+#     if M == 1:
+#         print(i)
+#     else:
+#         cache.append(i)
+
+# for n in permutations(cache, M):
+#     print(*n)
