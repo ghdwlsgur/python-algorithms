191484e09f9d204a9e88dec4726d69a638a2d17a
HongJinHyeok <redmax45@naver.com>
2022-07-04 12:09:26 +0900

    ♻️

--- backjoon/phase_14/5.py
+++ backjoon/phase_14/5.py
@@ -105,6 +105,28 @@
 #     print(i, end=" ")
 
 
+# n = int(sys.stdin.readline().rstrip())
+# input = [int(sys.stdin.readline().rstrip()) for _ in range(n)]
+# l = []
+# for i in range(1, len(input)):
+#     l.append(input[i] - input[i - 1])
+
+# target = l[0]
+# for i in range(1, len(input) - 1):
+#     target = gcd(target, l[i])
+
+# result = []
+# for i in range(1, int(sqrt(target)) + 1):
+#     if target % i == 0:
+#         if i ** 2 == target:
+#             result.append(i)
+#         else:
+#             result += [i, target // i]
+# result.remove(1)
+
+# for i in sorted(result):
+#     print(i, end=" ")
+
 import sys
 from math import sqrt, gcd
 
@@ -132,3 +154,6 @@ result.remove(1)
 
 for i in sorted(result):
     print(i, end=" ")
+
+
+
