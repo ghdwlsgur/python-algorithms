a282e0bbd480ef9a70533357031c2f3d2b23f9ef
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-07 11:32:03 +0900

    wifi sniff

--- wifi/sniff.py
+++ wifi/sniff.py
@@ -1,16 +1,16 @@
-from scapy.all import Dot11, sniff
+# from scapy.all import Dot11, sniff
 
-iface = "wlan0mon"
-ap_list = []
-
-
-def packetHandler(packet):
-    if packet.haslayer(Dot11):
-        if packet.type == 0 and packet.subtype == 8:
-            if packet.addr2 not in ap_list:
-                ap_list.append(packet.addr2)
-                print("Access Point MAC: {} with SSID: {}".format(
-                    packet.addr2, packet.info))
+# iface = "wlan0mon"
+# ap_list = []
 
 
-sniff(iface=iface, prn=packetHandler)
+# def packetHandler(packet):
+#     if packet.haslayer(Dot11):
+#         if packet.type == 0 and packet.subtype == 8:
+#             if packet.addr2 not in ap_list:
+#                 ap_list.append(packet.addr2)
+#                 print("Access Point MAC: {} with SSID: {}".format(
+#                     packet.addr2, packet.info))
+
+
+# sniff(iface=iface, prn=packetHandler)
