8c57e99ce02bcf77dd14fbc3e4565fe010169fd7
HongJinHyeok <vjinhyeokv@gmail.com>
2023-07-25 11:42:53 +0900

    공원 산책 코드 복습

--- programmers/level-1/3.py
+++ programmers/level-1/3.py
@@ -1,4 +1,44 @@
 
+
+def solution2(park, routes):
+    n, m = len(park), 0
+    x, y = 0, 0
+
+    block = []
+    for i in range(n):
+        for j, zero in enumerate(list(park[i])):
+            if zero == 'S':
+                x, y = i, j
+                m = len(list(park[i]))
+            elif zero == 'X':
+                block.append((i, j))
+    for d in routes:
+        d = d.split()
+        direction, distance = d[0], int(d[1])
+
+        checkpoint = (x, y)
+        for _ in range(distance):
+            if direction == 'E':
+                nx, ny = x, y + 1
+            elif direction == 'W':
+                nx, ny = x, y - 1
+            elif direction == 'S':
+                nx, ny = x + 1, y
+            elif direction == 'N':
+                nx, ny = x - 1, y
+
+            if nx < 0 or ny < 0 or nx >= m or ny >= m:
+                x, y = checkpoint[0], checkpoint[1]
+                break
+
+            if len(block) > 0:
+                if (nx, ny) in block:
+                    x, y = checkpoint[0], checkpoint[1]
+                    break
+            x, y = nx, ny
+    return [x, y]
+
+
 def solution(park, routes):
     n, m = len(park), 0
     x, y = 0, 0
@@ -45,6 +85,6 @@ def solution(park, routes):
 
 
 
-print(solution(["SOO","OOO","OOO"], ["E 2","S 2","W 1"]))
-print(solution(["SOO","OXX","OOO"], ["E 2","S 2","W 1"]))
-print(solution(["OSO","OOO","OXO","OOO"], ["E 2","S 3","W 1"]))
+print(solution2(["SOO","OOO","OOO"], ["E 2","S 2","W 1"]))
+print(solution2(["SOO","OXX","OOO"], ["E 2","S 2","W 1"]))
+print(solution2(["OSO","OOO","OXO","OOO"], ["E 2","S 3","W 1"]))
